8d793eb617c91d2f18aa4e2dd7d042a6b86386da
nathkalantar <143037154+nathkalantar@users.noreply.github.com>
2023-12-06 11:34:21 -0500

    Nivel 1 configurado

---
+++ Assets/Scripts/Triggers/DoorTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private bool DoorOpen = false;
+    private bool playerInRange;
+    public Transform DoorBisagra;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si se oprime el boton de interaccion
+        if (InputManager.GetInstance().GetInteractPressed() && DoorOpen == false)
+        {
+            DoorBisagra.rotation = new Quaternion(0, 180, 0, 0);
+            DoorOpen = true;
+        }
+        else if (InputManager.GetInstance().GetInteractPressed() && DoorOpen == true) 
+        {
+            DoorBisagra.rotation = new Quaternion(0, 90, 0, 0);
+            DoorOpen = false;
+        }
+
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        //Si el Objeto colisionado tiene la etiqueta de PLAYER -> Activamos Flag
+        playerInRange = collision.gameObject.CompareTag("Player") ? true : false;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        //Si el Objeto colisionado tiene la etiqueta de PLAYER -> Desactivamos flag
+        playerInRange = collision.gameObject.CompareTag("Player") ? false : true;
+    }
+}
